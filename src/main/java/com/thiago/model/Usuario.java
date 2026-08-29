c5f196fc2e2ce12928689822ee9e731186f45296
Thiago Gabriel <thiagogabrieel900@gmail.com>
2026-08-28 21:06:32 -0300

    add os atributos da class e o construtor

--- src/main/java/com/thiago/model/Usuario.java
+++ src/main/java/com/thiago/model/Usuario.java
@@ -1,4 +1,15 @@
 package com.thiago.model;
 
 public class Usuario {
+    private long id;
+    private String nome;
+    private String email;
+    private String senha;
+
+    public Usuario(long id,  String nome, String email, String senha) {
+        this.id = id;
+        this.nome = nome;
+        setEmail(email);
+        setSenha(senha);
+    }
 }
