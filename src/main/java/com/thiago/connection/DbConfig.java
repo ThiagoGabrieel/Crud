48c6b0caa6903711d1654f8eef81ae15bb5c9eae
Thiago Gabriel <thiagogabrieel900@gmail.com>
2026-08-29 14:00:45 -0300

    add metodo que retorna qual propriedade não foi encontrada. Nome das propriedades trocadas para serem iguais as registradas no arquivo db.properties

--- src/main/java/com/thiago/connection/DbConfig.java
+++ src/main/java/com/thiago/connection/DbConfig.java
@@ -8,10 +8,10 @@ public class DbConfig {
     private static final Properties props = new Properties();
 
     static {
-        try(InputStream input = DbConfig.class.getClassLoader()
-                .getResourceAsStream("db.properties")){
+        try (InputStream input = DbConfig.class.getClassLoader()
+                .getResourceAsStream("db.properties")) {
 
-            if(input == null){
+            if (input == null) {
                 throw new RuntimeException(
                         "Arquivo db.properties não encontrado. Copie db.properties.example e preencha os dados."
                 );
@@ -22,14 +22,24 @@ public class DbConfig {
             throw new RuntimeException("Erro ao carregar db.properties");
         }
     }
-    public static String getUrl(){
+
+    public static String getUrl() {
         return props.getProperty("db.url");
     }
 
-    public static String getUsername(){
-        return props.getProperty("db.username");
+    public static String getUser() {
+        return props.getProperty("db.user");
     }
-    public static String getPassword(){
+
+    public static String getPassword() {
         return props.getProperty("db.password");
+    }
+
+    private static String getRequiredProperty(String key) {
+        String value = props.getProperty(key);
+        if (value == null) {
+            throw new RuntimeException("Propriedade obrigatoria não encontrada: " + key);
+        }
+        return value;
     }
 }
