334d18ec20261531722bf710fdd59b0e0d572a5c
Thiago Gabriel <thiagogabrieel900@gmail.com>
2026-08-28 21:12:21 -0300

    add os getters e setters com configuração no set de senha e email

--- src/main/java/com/thiago/model/Usuario.java
+++ src/main/java/com/thiago/model/Usuario.java
@@ -12,4 +12,25 @@ public class Usuario {
         setEmail(email);
         setSenha(senha);
     }
+
+    public long getId() { return id; }
+    public String getNome() { return nome; }
+    public String getEmail() { return email; }
+    public String getSenha() { return senha; }
+
+    public void setId(long id) { this.id = id; }
+    public void setNome(String nome) { this.nome = nome; }
+
+    public void setEmail(String email) {
+        if(email == null || !email.matches("[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}")){
+            throw new IllegalArgumentException("Email inválido");
+        }
+        this.email = email;
+    }
+    public void setSenha(String senha) {
+        if(senha == null || senha.matches("[a-zA-Z0-9@#]")){
+            throw new IllegalArgumentException("Senha inválida");
+        }
+        this.senha = senha;
+    }
 }
