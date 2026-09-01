2dbcde34573a53bea90780de5cd2ae4cdaf4f39b
Thiago Gabriel <thiagogabrieel900@gmail.com>
2026-08-31 21:18:13 -0300

    Definido regras para atualização de email

--- src/main/java/com/thiago/service/UsuarioService.java
+++ src/main/java/com/thiago/service/UsuarioService.java
@@ -33,4 +33,23 @@ public class UsuarioService {
         }
         return encontrado;
     }
+
+    public Usuario atualizarEmail(long id, String email, String senha){
+        Usuario usuario = repository.buscarPorId(id);
+
+        if(usuario == null){
+            throw new IllegalArgumentException("Usuario não encontrado!");
+        }
+        if(!usuario.verificarSenha(senha)) {
+            throw new IllegalArgumentException("Senha incorreta!");
+        }
+        if(repository.emailJaExistente(email)){
+            throw new IllegalArgumentException("Invalido. Esse email já foi cadastrado!");
+        }
+
+        repository.atualizarEmail(id, email);
+        System.out.println("Email atualizado com sucesso!");
+
+        return repository.buscarPorId(id);
+    }
 }
