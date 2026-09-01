7372a93835c1d6b61f46a8c50e784312e1b6ebc3
Thiago Gabriel <thiagogabrieel900@gmail.com>
2026-08-31 21:34:52 -0300

    Menu inicial estruturada com while e switch

--- src/main/java/com/thiago/Main.java
+++ src/main/java/com/thiago/Main.java
@@ -1,7 +1,36 @@
 package com.thiago;
 
+import com.thiago.model.Usuario;
+import com.thiago.repository.UsuarioRepository;
+import com.thiago.service.UsuarioService;
+
+import java.util.Scanner;
+
 public class Main {
+    static Scanner sc = new Scanner(System.in);
+    static UsuarioService service = new UsuarioService();
+    static Usuario usuario;
+    static UsuarioRepository repository = new UsuarioRepository();
+
     public static void main(String[] args) {
 
+        while(true){
+            System.out.println("----- MENU INICIAL -----");
+            System.out.println("[1] - CADASTRAR");
+            System.out.println("[2] - LOGIN");
+            System.out.println("[3] - EXIT");
+            System.out.println("-------------------------");
+            int opcao = sc.nextInt();
+
+            switch (opcao){
+                case 1: cadastrar(); break;
+                case 2: login(); break;
+                case 3: System.out.print("Encerrando...");
+                    sc.close();
+                    return;
+
+                default: System.out.println("Opção inválida");
+            }
+        }
     }
 }
