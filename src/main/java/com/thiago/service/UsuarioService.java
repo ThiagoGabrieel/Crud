1b3c4fd594abc69db411b5ad970b0d62a3dd1024
Thiago Gabriel <thiagogabrieel900@gmail.com>
2026-08-31 21:08:26 -0300

    adicionado as regras de negocio para validar o registro

--- src/main/java/com/thiago/service/UsuarioService.java
+++ src/main/java/com/thiago/service/UsuarioService.java
@@ -1,4 +1,24 @@
 package com.thiago.service;
 
+import com.thiago.model.Usuario;
+import com.thiago.repository.UsuarioRepository;
+
 public class UsuarioService {
+    private UsuarioRepository repository = new UsuarioRepository();
+
+    public Usuario cadastrar(String nome, String email, String senha){
+
+        if(repository.emailJaExistente(email)){
+            throw new IllegalArgumentException("Email já Existente no momento!");
+        }
+        if(senha == null || !senha.matches("[a-zA-Z0-9@#]{10}")){
+            throw new IllegalArgumentException("Senha invalida");
+        }
+
+        Usuario Novousuario = new Usuario(nome, email, senha);
+        repository.salvar(Novousuario);
+
+        System.out.println("Usuario cadastrado com sucesso!");
+        return Novousuario;
+    }
 }
