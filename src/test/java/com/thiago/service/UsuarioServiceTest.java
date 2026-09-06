47a1eb6d76413bc7ca1675a6c924696ca378efcc
Thiago Gabriel <thiagogabrieel900@gmail.com>
2026-09-06 14:11:05 -0300

    puxado extensão do mockito para class de teste e os imports necessarios

---
+++ src/test/java/com/thiago/service/UsuarioServiceTest.java
@@ -0,0 +1,18 @@
+package com.thiago.service;
+
+public class UsuarioService {
+import com.thiago.model.Usuario;
+import com.thiago.repository.UsuarioRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
+@ExtendWith(MockitoExtension.class)
+public class UsuarioServiceTest {
+
+}
